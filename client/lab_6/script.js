9e289e50e430dc08cab0da7f836416bdf7733872
noruga12 <71854937+noruga12@users.noreply.github.com>
2022-10-29 00:14:31 -0400

    implement lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -12,7 +12,7 @@
 */
 
 function injectHTML(list) {
-  console.log('fired injectHTML');
+  console.log("fired injectHTML");
   /*
   ## JS and HTML Injection
     There are a bunch of methods to inject text or HTML into a document using JS
@@ -27,10 +27,24 @@ function injectHTML(list) {
     - using a .forEach method, inject a list element into your index.html for every element in the list
     - Display the name of that restaurant and what category of food it is
 */
+
+  const rightBoxDiv = document.getElementById("right_section_box");
+  let result = "";
+
+  list.forEach((res) => {
+    result += `<li>${res.name}. Location: ${res.location}</li>`;
+  });
+
+  rightBoxDiv.setHTML(`
+  <div>
+        <ul>
+            ${result}
+        </ul>
+  </div>`);
 }
 
 function processRestaurants(list) {
-  console.log('fired restaurants list');
+  console.log("fired restaurants list");
 
   /*
     ## Process Data Separately From Injecting It
@@ -50,6 +64,31 @@ function processRestaurants(list) {
     - Return only their name, category, and location
     - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
   */
+
+  const result = [...Array(15)].map((u) => ({}));
+  let n = 0;
+  const len = list.length;
+  const taken = new Set();
+
+  while (n < 15) {
+    const i = Math.floor(Math.random() * len);
+
+    if (taken.has(i)) {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+    taken.add(i);
+    result[n] = {
+      name: list[i].name,
+      category: list[i].category,
+      location: list[i].address_line_1,
+    };
+
+    // eslint-disable-next-line no-plusplus
+    n++;
+  }
+
+  return result;
 }
 
 async function mainEvent() {
@@ -61,16 +100,17 @@ async function mainEvent() {
   */
 
   // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
+  const form = document.querySelector(".main_form"); // get your main form so you can do JS with it
   const submit = document.querySelector('button[type="submit"]'); // get a reference to your submit button
-  submit.style.display = 'none'; // let your submit button disappear
+  const loader = document.getElementById('lds-ellipsis');
+  submit.style.display = "none"; // let your submit button disappear
 
   /*
     Let's get some data from the API - it will take a second or two to load
     This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
     It's at about line 27 - go have a look and see what we're retrieving and sending back.
    */
-  const results = await fetch('/api/foodServicesPG');
+  const results = await fetch("/api/foodServicePG");
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
   /*
@@ -86,15 +126,19 @@ async function mainEvent() {
   console.log(arrayFromJson.data[0]);
 
   // this is called "string interpolation" and is how we build large text blocks with variables
-  console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
+  console.log(
+    `${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`
+  );
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
-    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
+  if (arrayFromJson.data?.length > 0) {
+    // the question mark in this means "if this is set at all"
+    loader.style.display = "none";
+    submit.style.display = "block"; // let's turn the submit button back on by setting it to display as a block when we have data available
 
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    form.addEventListener('submit', (submitEvent) => {
+    form.addEventListener("submit", (submitEvent) => {
       // This is needed to stop our page from changing to a new URL even though it heard a GET request
       submitEvent.preventDefault();
 
@@ -116,4 +160,4 @@ async function mainEvent() {
   It's calling the 'mainEvent' function at line 57
   It runs first because the listener is set to when your HTML content has loaded
 */
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
